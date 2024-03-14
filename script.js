bd64545280deb78c10234fa39aab4184922dc1ca
garciavelezj <156826378+garciavelezj@users.noreply.github.com>
2024-03-14 13:06:14 -0400

    update to pages

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Get the button:
+//for scroll button
+
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,8 +12,31 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+//for my work buttons
+
+let all = document.getElementById("all");
+let design = document.getElementById("design");
+let dev = document.getElementById("dev");
+
+function showDesign() {
+  design.style.display = "block";
+  dev.style.display = "none";
+  design.style.display = "block";
+}
+
+function showDev() {
+    dev.style.display = "block";
+    design.style.display = "none";
+    design2.style.display = "none";
+}
+
+function showAll() {
+    dev.style.display = "block";
+    design.style.display = "block";
+    design2.style.display = "block";
 }
