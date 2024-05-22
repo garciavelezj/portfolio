69d2ad80f1001912df537de0022404ed4ffeb7f7
garciavelezj <156826378+garciavelezj@users.noreply.github.com>
2024-05-22 14:36:19 -0400

    update

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-//for scroll button
-
-let mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
 //for my work buttons
