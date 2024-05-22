e4eaf9800b5af4f7e35f0fd02f2910a6b8cadada
garciavelezj <156826378+garciavelezj@users.noreply.github.com>
2024-05-22 14:38:11 -0400

    update2

--- script.js
+++ script.js
@@ -4,27 +4,3 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-//for my work buttons
-
-let all = document.getElementById("all");
-let design = document.getElementById("design");
-let dev = document.getElementById("dev");
-
-function showDesign() {
-  design.style.display = "block";
-  dev.style.display = "none";
-  design.style.display = "block";
-}
-
-function showDev() {
-    dev.style.display = "block";
-    design.style.display = "none";
-    design2.style.display = "none";
-}
-
-function showAll() {
-    dev.style.display = "block";
-    design.style.display = "block";
-    design2.style.display = "block";
-}
